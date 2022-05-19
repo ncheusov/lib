97d43a39b53f71352cd5bf52c9a66ec385357c0e
Nikita Cheusov <ncheusov@inbox.ru>
2022-05-19 22:19:46 +0300

    modified actions.js

--- src/js/main.js
+++ src/js/main.js
@@ -8,6 +8,8 @@ $('div').click(function() {
     console.log($(this).index());
 })
 
-console.log($('div').eq(2).find('.more'));
+// console.log($('div').eq(2).find('.more'));
+// console.log($('.some').closest('.findme'));
+console.log($('.findme').siblings());
 
 // console.log($('button').html('Hello'));
