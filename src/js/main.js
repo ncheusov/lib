1ef34e000b4f9b54ab67c6d2070e970b76bf34dc
Nikita Cheusov <ncheusov@inbox.ru>
2022-05-22 23:01:18 +0300

    dynamic created modals

--- src/js/main.js
+++ src/js/main.js
@@ -11,3 +11,36 @@ $('[data-count="second"]').on('click', () => {
 $('button').eq(2).on('click', () => {
     $('.w-500').fadeToggle(800);
 });
+
+$('#trigger').click(() => $('#trigger').createModal({
+    text: {
+        title: 'Modal title',
+        body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum maiores voluptatum libero commodi nisi vitae porro quas incidunt dolor dolores. Labore numquam, quam perspiciatis velit sit beatae impedit aliquam libero!'
+    },
+    btns: {
+        count: 3,
+        settings: [
+            [
+                'Close',
+                ['btn-danger', 'mr-10'],
+                true
+            ],
+            [
+                'Save changes',
+                ['btn-success'],
+                false,
+                () => {
+                    alert('Данные сохранены');
+                }
+            ],
+            [
+                'Another button',
+                ['btn-warning', 'ml-10'],
+                false,
+                () => {
+                    alert('hello world');
+                }
+            ]
+        ]
+    }
+}));
